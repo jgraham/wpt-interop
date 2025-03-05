e935897e6cdf6b51ac48c9e9a266090c1946e66c
James Graham <james@hoppipolla.co.uk>
2025-03-05 13:52:53 +0000

    Add support for computing the interop score for Gecko runs.
    This gets the data out of a gecko results analysis cache, with a
    similar setup to the wpt.fyi results cache, and allows generating a
    set of CSV files specifically for these runs (they are considered
    separate to the normal interop runs since they're on different source
    commits &c.).

--- python/python/wpt_interop/runs.py
+++ python/python/wpt_interop/runs.py
@@ -3,8 +3,21 @@ import logging
 import os
 from datetime import datetime, timedelta
 from types import TracebackType
-from typing import Any, ContextManager, Iterable, Iterator, Mapping, MutableMapping, Optional, Union
+from typing import (
+    Any,
+    ContextManager,
+    Iterable,
+    Iterator,
+    Mapping,
+    MutableMapping,
+    MutableSequence,
+    Optional,
+    Sequence,
+)
 from urllib.parse import urlencode
+
+from .repo import ResultsAnalysisCache
+from ._wpt_interop import gecko_runs
 
 import requests
 
@@ -14,23 +27,21 @@ RunsByDate = Mapping[str, list["RevisionRuns"]]
 
 logger = logging.getLogger("wpt_interop.runs")
 
+Json = None | int | float | str | bool | Sequence["Json"] | Mapping[str, "Json"]
+
 
 class Run:
     def __init__(
         self,
-        run_id: int,
+        run_id: str,
         browser_name: str,
         browser_version: str,
         os_name: str,
         os_version: str,
         revision: str,
         full_revision_hash: str,
-        results_url: str,
         created_at: datetime,
         time_start: datetime,
-        time_end: datetime,
-        raw_results_url: str,
-        labels: list[str],
     ):
         self.run_id = run_id
         self.browser_name = browser_name
@@ -39,32 +50,42 @@ class Run:
         self.os_version = os_version
         self.revision = revision
         self.full_revision_hash = full_revision_hash
-        self.results_url = results_url
         self.created_at = created_at
         self.time_start = time_start
-        self.time_end = time_end
-        self.raw_results_url = raw_results_url
-        self.labels = labels
 
     @classmethod
-    def from_json(cls, data: Mapping[str, Any]) -> "Run":
+    def from_json(cls, data: Mapping[str, Json]) -> "Run":
+        run_id = data["id"]
+        assert isinstance(run_id, str)
+        browser_name = data["browser_name"]
+        assert isinstance(browser_name, str)
+        browser_version = data["browser_version"]
+        assert isinstance(browser_version, str)
+        os_name = data["os_name"]
+        assert isinstance(os_name, str)
+        os_version = data["os_version"]
+        assert isinstance(os_version, str)
+        revision = data["revision"]
+        assert isinstance(revision, str)
+        full_revision_hash = data["full_revision_hash"]
+        assert isinstance(full_revision_hash, str)
+        created_at = data["created_at"]
+        assert isinstance(created_at, str)
+        time_start = data["time_start"]
+        assert isinstance(time_start, str)
         return cls(
-            data["id"],
-            data["browser_name"],
-            data["browser_version"],
-            data["os_name"],
-            data["os_version"],
-            data["revision"],
-            data["full_revision_hash"],
-            data["results_url"],
-            datetime.fromisoformat(data["created_at"]),
-            datetime.fromisoformat(data["time_start"]),
-            datetime.fromisoformat(data["time_end"]),
-            data["raw_results_url"],
-            data["labels"],
+            run_id,
+            browser_name,
+            browser_version,
+            os_name,
+            os_version,
+            revision,
+            full_revision_hash,
+            datetime.fromisoformat(created_at),
+            datetime.fromisoformat(time_start),
         )
 
-    def to_json(self) -> Mapping[str, Union[str, int, list[str]]]:
+    def to_json(self) -> MutableMapping[str, Json]:
         return {
             "id": self.run_id,
             "browser_name": self.browser_name,
@@ -73,23 +94,206 @@ class Run:
             "os_version": self.os_version,
             "revision": self.revision,
             "full_revision_hash": self.full_revision_hash,
-            "results_url": self.results_url,
             "created_at": self.created_at.isoformat(),
             "time_start": self.time_start.isoformat(),
-            "time_end": self.time_end.isoformat(),
-            "raw_results_url": self.raw_results_url,
-            "labels": self.labels,
         }
+
+
+class WptFyiRun(Run):
+    def __init__(
+        self,
+        run_id: str,
+        browser_name: str,
+        browser_version: str,
+        os_name: str,
+        os_version: str,
+        revision: str,
+        full_revision_hash: str,
+        created_at: datetime,
+        time_start: datetime,
+        time_end: datetime,
+        results_url: str,
+        raw_results_url: str,
+        labels: list[str],
+    ):
+        super().__init__(
+            run_id,
+            browser_name,
+            browser_version,
+            os_name,
+            os_version,
+            revision,
+            full_revision_hash,
+            created_at,
+            time_start,
+        )
+        self.time_end = time_end
+        self.results_url = results_url
+        self.raw_results_url = raw_results_url
+        self.labels = labels
+
+    @classmethod
+    def from_json(cls, data: Mapping[str, Json]) -> "WptFyiRun":
+        run_id = str(data["id"])
+        browser_name = data["browser_name"]
+        assert isinstance(browser_name, str)
+        browser_version = data["browser_version"]
+        assert isinstance(browser_version, str)
+        os_name = data["os_name"]
+        assert isinstance(os_name, str)
+        os_version = data["os_version"]
+        assert isinstance(os_version, str)
+        revision = data["revision"]
+        assert isinstance(revision, str)
+        full_revision_hash = data["full_revision_hash"]
+        assert isinstance(full_revision_hash, str)
+        created_at = data["created_at"]
+        assert isinstance(created_at, str)
+        time_start = data["time_start"]
+        assert isinstance(time_start, str)
+        time_end = data["time_end"]
+        assert isinstance(time_end, str)
+        results_url = data["results_url"]
+        assert isinstance(results_url, str)
+        raw_results_url = data["raw_results_url"]
+        assert isinstance(raw_results_url, str)
+        labels = data["labels"]
+        assert isinstance(labels, list)
+        for item in labels:
+            assert isinstance(item, str)
+        return cls(
+            run_id,
+            browser_name,
+            browser_version,
+            os_name,
+            os_version,
+            revision,
+            full_revision_hash,
+            datetime.fromisoformat(created_at),
+            datetime.fromisoformat(time_start),
+            datetime.fromisoformat(time_end),
+            results_url,
+            raw_results_url,
+            labels,
+        )
+
+    def to_json(self) -> MutableMapping[str, Json]:
+        rv = super().to_json()
+        rv.update(
+            {
+                "results_url": self.results_url,
+                "time_end": self.time_end.isoformat(),
+                "raw_results_url": self.raw_results_url,
+                "labels": self.labels,
+            }
+        )
+        return rv
+
+
+class GeckoRun(Run):
+    def __init__(
+        self,
+        run_id: str,
+        browser_name: str,
+        browser_version: str,
+        os_name: str,
+        os_version: str,
+        revision: str,
+        full_revision_hash: str,
+        created_at: datetime,
+        time_start: datetime,
+        run_info: Json,
+    ):
+        super().__init__(
+            run_id,
+            browser_name,
+            browser_version,
+            os_name,
+            os_version,
+            revision,
+            full_revision_hash,
+            created_at,
+            time_start,
+        )
+        self.run_info = run_info
+
+    @classmethod
+    def from_run(
+        cls, commit: str, push_date: datetime, run_id: str, run_info: Mapping[str, Json]
+    ) -> "GeckoRun":
+        browser = run_info["product"]
+        assert isinstance(browser, str)
+        browser_version = run_info["browser_build_id"]
+        assert isinstance(browser_version, str)
+        os_name = run_info["os"]
+        assert isinstance(os_name, str)
+        os_version = run_info["os_version"]
+        assert isinstance(os_version, str)
+
+        return cls(
+            run_id,
+            browser,
+            browser_version,
+            os_name,
+            os_version,
+            commit,
+            commit,
+            push_date,
+            push_date,
+            run_info,
+        )
+
+    @classmethod
+    def from_json(cls, data: Mapping[str, Json]) -> "GeckoRun":
+        run_id = data["id"]
+        assert isinstance(run_id, str)
+        browser_name = data["browser_name"]
+        assert isinstance(browser_name, str)
+        browser_version = data["browser_version"]
+        assert isinstance(browser_version, str)
+        os_name = data["os_name"]
+        assert isinstance(os_name, str)
+        os_version = data["os_version"]
+        assert isinstance(os_version, str)
+        revision = data["revision"]
+        assert isinstance(revision, str)
+        full_revision_hash = data["full_revision_hash"]
+        assert isinstance(full_revision_hash, str)
+        created_at = data["created_at"]
+        assert isinstance(created_at, str)
+        time_start = data["time_start"]
+        assert isinstance(time_start, str)
+        run_info = data["run_info"]
+        return cls(
+            run_id,
+            browser_name,
+            browser_version,
+            os_name,
+            os_version,
+            revision,
+            full_revision_hash,
+            datetime.fromisoformat(created_at),
+            datetime.fromisoformat(time_start),
+            run_info,
+        )
+
+    def to_json(self) -> MutableMapping[str, Json]:
+        rv = super().to_json()
+        rv["run_info"] = self.run_info
+        return rv
 
 
 class RevisionRuns:
     # Runs for a specific revision
-    def __init__(self, revision: str, runs: list[Run]):
+    def __init__(self, revision: str, runs: MutableSequence[Run]):
         self.revision = revision
         self.runs = runs
 
     def __len__(self) -> int:
         return len(self.runs)
+
+    def __bool__(self) -> bool:
+        return bool(self.runs)
 
     def __iter__(self) -> Iterator[Run]:
         yield from self.runs
@@ -104,7 +308,7 @@ class RevisionRuns:
     def min_start_time(self) -> datetime:
         return min(item.time_start for item in self.runs)
 
-    def run_ids(self) -> list[int]:
+    def run_ids(self) -> list[str]:
         return [item.run_id for item in self.runs]
 
     def is_aligned(self, products: list[str]) -> bool:
@@ -149,7 +353,48 @@ def group_by_date(runs_by_revision: RunsByRevision) -> RunsByDate:
     return runs_by_date
 
 
-def fetch_runs(
+def run_info_filter_matches(
+    run_info_filter: Mapping[str, Json], run_info: Mapping[str, Json]
+) -> bool:
+    for key, value in run_info_filter.items():
+        if key in run_info and run_info[key] != value:
+            return False
+    return True
+
+
+def fetch_runs_gecko(
+    results_analysis_repo: ResultsAnalysisCache,
+    run_info_filter: Mapping[str, Json],
+    from_date: Optional[datetime] = None,
+    to_date: Optional[datetime] = None,
+) -> RunsByRevision:
+    now = datetime.now()
+    if from_date is None:
+        from_date = datetime(now.year, 1, 1)
+    if to_date is None:
+        to_date = datetime(now.year, now.month, now.day)
+
+    rv = []
+    for date_commits in gecko_runs(
+        results_analysis_repo.path, "mozilla-central", from_date, to_date
+    ).values():
+        for commit, commit_runs in date_commits.items():
+            revision_runs = RevisionRuns(commit, [])
+            push_date = commit_runs.push_date
+            for run_data in commit_runs.runs.values():
+                if run_info_filter_matches(run_info_filter, run_data.run_info):
+                    revision_runs.append(
+                        GeckoRun.from_run(commit, push_date, run_data.id, run_data.run_info)
+                    )
+            if revision_runs:
+                rv.append(revision_runs)
+                # TODO: remove this
+                assert len(revision_runs) == 1
+
+    return RunsByRevision(rv)
+
+
+def fetch_runs_wptfyi(
     products: list[str],
     channel: str,
     from_date: Optional[datetime] = None,
@@ -221,10 +466,10 @@ def fetch_runs(
     return RunsByRevision(rv)
 
 
-def group_by_revision(runs: list[Mapping[str, Any]]) -> Mapping[str, list[Run]]:
-    rv: dict[str, list[Run]] = {}
+def group_by_revision(runs: list[Mapping[str, Any]]) -> Mapping[str, list[WptFyiRun]]:
+    rv: dict[str, list[WptFyiRun]] = {}
     for run_json in runs:
-        run = Run.from_json(run_json)
+        run = WptFyiRun.from_json(run_json)
         if run.full_revision_hash not in rv:
             rv[run.full_revision_hash] = []
         rv[run.full_revision_hash].append(run)
